75cdecf65bf0e41ffd7ef8650307c4f7d1807d32
Nur-NobyRahaman <github943@gmail.com>
2022-06-26 20:18:36 +0600

    first commit

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 function gitButton(){
    location.href="https://github.com/Nur-NobyRahaman/developer-protdfolio";
 }
+function facebook(){
+   location.href="https://www.facebook.com/nur.noby.2588/";
+}
+function Linkedin(){
+   location.href="https://www.linkedin.com/in/nur-noby-40293b182/";
+}
+function Skype(){
+   location.href="https://github.com/Nur-NobyRahaman/developer-protdfolio";
+}
+function Youtube(){
+   location.href="https://github.com/Nur-NobyRahaman/developer-protdfolio";
+}
